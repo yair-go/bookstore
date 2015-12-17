819f1dd819c15e3768fc7a14c63a3ed3151df680
yairJCT <irigo1@gmail.com>
2015-12-17 13:04:55 +0200

    v5 book, invitation

--- app/src/main/java/com/yair/bookstore/Book.java
+++ app/src/main/java/com/yair/bookstore/Book.java
@@ -1,1 +1,35 @@
+package com.yair.bookstore;
 
+/**
+ * Created by Yair on 13/12/2015.
+ */
+public class Book {
+    private int _id;
+    private String _name;
+    private int image;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void set_name(String _name) {
+        this._name = _name;
+    }
+
+}
---
+++ app/src/main/java/com/yair/bookstore/Invitation.java
@@ -0,0 +1,7 @@
+package com.yair.bookstore;
+
+/**
+ * Created by Yair on 13/12/2015.
+ */
+public class Invitation {
+}
